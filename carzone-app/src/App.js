3990f375ffeebf9bd6ed140a39620f075780a501
fahmi-aldairi <fahmialdairi10@gmail.com>
2023-05-07 01:25:38 +0300

    submit products page

--- carzone-app/src/App.js
+++ carzone-app/src/App.js
@@ -8,32 +8,27 @@ import Gallery from "./component/Gallery";
 import Registration from "./component/Registration";
 import Cart from "./component/Cart";
 import About from "./component/About";
-import Contact from "./component/Contact"; 
+import Contact from "./component/Contact";
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function App() {
-
   const [brand, setBrand] = useState("toyota");
-   
-
 
   return (
     <>
       <BrowserRouter>
-
-      <Nav/>
+        <Nav />
         <Routes>
           <Route index element={<Main setBrand={setBrand} />} />
-          <Route path="Products" index element={<Products brand={brand}/>} />
+          <Route path="Products" index element={<Products brand={brand} />} />
           <Route path="Gallery" index element={<Gallery />} />
           <Route path="Registration" index element={<Registration />} />
           <Route path="Cart" index element={<Cart />} />
           <Route path="Contact" index element={<Contact />} />
           <Route path="About" index element={<About />} />
         </Routes>
-        <Footer/>
-
+        <Footer />
       </BrowserRouter>
     </>
   );
