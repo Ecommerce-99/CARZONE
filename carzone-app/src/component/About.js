7e05dc77c9d0e6054535d95484051b378978ec7a
fahmi-aldairi <fahmialdairi10@gmail.com>
2023-05-08 00:54:10 +0300

    Update about-us

--- carzone-app/src/component/About.js
+++ carzone-app/src/component/About.js
@@ -1,105 +1,236 @@
 import React from "react";
-import "../component/About.css"
+import "../component/About.css";
 
 const About = () => {
   return (
-    <div>
-      <>
-        {/* Masthead*/}
-        <header className="masthead">
-          <div className="container">
-            <div className="masthead-subheading">
-              <h1 className="welcome">
-                Welcome To <span> Car Zone!</span>{" "}
-              </h1>
+    <>
+      <section id="about" className="about">
+        <div className="container-md jus" data-aos="fade-up">
+          <div className="row position-relative">
+            <div className="col-lg-7 about-img" />
+            <div className="col-lg-7">
+              <h2>Luxury Cars</h2>
+              <div className="our-story">
+                <h3>Our vision</h3>
+                <p>
+                  Our vision is to be the UK's favourite automotive retailer.
+                  That doesn't just mean you'll get the best experience when you
+                  buy a car from us, but when servicing, maintaining and selling
+                  your car, too. We believe every part of your car ownership
+                  experience should be as tranquil as possible. We'll empower
+                  you to make the right decision for you with helpful advice,
+                  total transparency and service that's flexible to your needs.
+                </p>
+                <h3>Our mission</h3>
+                <p>
+                  Our vision is to be the UK's favourite automotive retailer.
+                  That doesn't just mean you'll get the best experience when you
+                  buy a car from us, but when servicing, maintaining and selling
+                  your car, too. We believe every part of your car ownership
+                  experience should be as tranquil as possible. We'll empower
+                  you to make the right decision for you with helpful advice,
+                  total transparency and service that's flexible to your needs.
+                </p>
+              </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section id="team" class="team">
+        <div class="container mb-5" data-aos="fade-up">
+          <div class="section-header">
+            <h2>Our Team</h2>
             <p>
-              {" "}
-              CAR ZONE Is A Platform Designed To Help People and car showrooms
-              to Get The Services That Are Required Related to buying, selling,
-              and investing in cars by finding the easiest way to find your
-              dreams car{" "}
+              Aperiam dolorum et et wuia molestias qui eveniet numquam nihil
+              porro incidunt dolores placeat sunt id nobis omnis tiledo stran
+              delop
             </p>
-            <a
-              className="btn btn-primary btn-xl text-uppercase"
-              href="#services"
-            >
-              Tell Me More
-            </a>
           </div>
-        </header>
-        {/* Team*/}
-        <section className="page-section bg-white" id="team">
-          <div className="container">
-            <div className="text-center">
-              <h2 className="section-heading text-uppercase">
-                Meet Our Amazing Team
-              </h2>
-              <h3 className="section-subheading text-muted"></h3>
-            </div>
-            <div className="row">
-              <div className="col-lg-4">
-                <div className="team-member">
-                  <img
-                    className="mx-auto rounded-circle"
-                    src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/team/3.jpg"
-                    alt="..."
-                  />
-                  <h4>Parveen Anand</h4>
-                  <p className="text-muted">Lead Designer</p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="team-member">
-                  <img
-                    className="mx-auto rounded-circle"
-                    src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/team/2.jpg"
-                    alt="..."
-                  />
-                  <h4>Diana Petersen</h4>
-                  <p className="text-muted">Lead Marketer</p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="team-member">
-                  <img
-                    className="mx-auto rounded-circle"
-                    src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/team/1.jpg"
-                    alt="..."
-                  />
-                  <h4>Larry Parker</h4>
-                  <p className="text-muted">Lead Developer</p>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-lg-">
-                <div className="team-member">
-                  <img
-                    className="mx-auto rounded-circle"
-                    src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/team/3.jpg"
-                    alt="..."
-                  />
-                  <h4>Parveen Anand</h4>
-                  <p className="text-muted1">Lead Designer</p>
-                </div>
-              </div>
 
-              <div className="col-lg-3 ">
-                <div className="team-member">
-                  <img
-                    className="mx-auto rounded-circle"
-                    src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/team/2.jpg"
-                    alt="..."
-                  />
-                  <h4>Diana Petersen</h4>
+          <div class="row justify-content-center gy-5">
+            <div
+              class="col-lg-4 col-md-6 member"
+              data-aos="fade-up"
+              data-aos-delay="100"
+            >
+              <div class="member-img">
+                <img
+                  src="https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png"
+                  class="img-fluid"
+                  alt=""
+                />
+                <div class="social">
+                  <a href="#">
+                    <i class="bi bi-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-facebook"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-linkedin"></i>
+                  </a>
                 </div>
+              </div>
+              <div class="member-info text-center">
+                <h4>Walter White</h4>
+                <span>Chief Executive Officer</span>
+                <p>
+                  Aliquam iure quaerat voluptatem praesentium possimus unde
+                  laudantium vel dolorum distinctio dire flow
+                </p>
+              </div>
+            </div>
+
+            <div
+              class="col-lg-4 col-md-6 member"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
+              <div class="member-img">
+                <img
+                  src="https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png"
+                  class="img-fluid"
+                  alt=""
+                />
+                <div class="social">
+                  <a href="#">
+                    <i class="bi bi-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-facebook"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-linkedin"></i>
+                  </a>
+                </div>
+              </div>
+              <div class="member-info text-center">
+                <h4>Sarah Jhonson</h4>
+                <span>Product Manager</span>
+                <p>
+                  Labore ipsam sit consequatur exercitationem rerum laboriosam
+                  laudantium aut quod dolores exercitationem ut
+                </p>
+              </div>
+            </div>
+
+            <div
+              class="col-lg-4 col-md-6 member"
+              data-aos="fade-up"
+              data-aos-delay="300"
+            >
+              <div class="member-img">
+                <img
+                  src="https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png"
+                  class="img-fluid"
+                  alt=""
+                />
+                <div class="social">
+                  <a href="#">
+                    <i class="bi bi-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-facebook"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-linkedin"></i>
+                  </a>
+                </div>
+              </div>
+              <div class="member-info text-center">
+                <h4>William Anderson</h4>
+                <span>CTO</span>
+                <p>
+                  Illum minima ea autem doloremque ipsum quidem quas aspernatur
+                  modi ut praesentium vel tque sed facilis at qui
+                </p>
+              </div>
+            </div>
+
+            <div
+              class="col-lg-4 col-md-6 member"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
+              <div class="member-img">
+                <img
+                  src="https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png"
+                  class="img-fluid"
+                  alt=""
+                />
+                <div class="social">
+                  <a href="#">
+                    <i class="bi bi-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-facebook"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-linkedin"></i>
+                  </a>
+                </div>
+              </div>
+              <div class="member-info text-center">
+                <h4>Amanda Jepson</h4>
+                <span>Accountant</span>
+                <p>
+                  Magni voluptatem accusamus assumenda cum nisi aut qui dolorem
+                  voluptate sed et veniam quasi quam consectetur
+                </p>
+              </div>
+            </div>
+
+            <div
+              class="col-lg-4 col-md-6 member"
+              data-aos="fade-up"
+              data-aos-delay="500"
+            >
+              <div class="member-img">
+                <img
+                  src="https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png"
+                  class="img-fluid"
+                  alt=""
+                />
+                <div class="social">
+                  <a href="#">
+                    <i class="bi bi-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-facebook"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i class="bi bi-linkedin"></i>
+                  </a>
+                </div>
+              </div>
+              <div class="member-info text-center">
+                <h4>Brian Doe</h4>
+                <span>Marketing</span>
+                <p>
+                  Qui consequuntur quos accusamus magnam quo est molestiae eius
+                  laboriosam sunt doloribus quia impedit laborum velit
+                </p>
               </div>
             </div>
           </div>
-        </section>
-      </>
-    </div>
+        </div>
+      </section>
+    </>
   );
 };
 
